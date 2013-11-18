2ed4a04ef3b9d2683cc44599defd19ab27c7951f
Scott Kuehn <scott.kuehn@opower.com>
2013-11-17 16:09:25 -0800

    config fixes, upgrade hadoop version

---
+++ camus-example/src/main/scripts/launch-camus.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+USAGE="$0 kafka-root-dir kafka-broker-str"
+
+if [ $# -ne 2 ]; then
+    echo ${USAGE}
+    exit 1
+fi
+
+KAFKA_HDFS_ROOT=$1
+BROKER=$2
+JAR=target/camus-example-*-SNAPSHOT-shaded.jar
+
+# /user/scott.kuehn/kafka-etl/history
+
+hadoop jar \
+    ${JAR} \
+    -p camus.properties \
+    -D etl.destination.path=${KAFKA_HDFS_ROOT}/data-output \
+    -D etl.execution.base.path=${KAFKA_HDFS_ROOT} \
+    -D etl.execution.history.path=${KAFKA_HDFS_ROOT}/history \
+    -D kafka.brokers=${BROKER}
